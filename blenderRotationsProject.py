2a4a5b555e6de03f2f0cce67a722937cedf6cae8
Random-Rishabh <89916315+Random-Rishabh@users.noreply.github.com>
2022-07-28 15:07:18 +0530

    Add files via upload

---
+++ blenderRotationsProject.py
@@ -0,0 +1,45 @@
+
+import bpy
+from json import load
+original_json=open(r"C:\Users\HP\Desktop\blender_python\Drome.json")
+dict_json=load(original_json)
+list_actions=list(dict_json.keys())
+n=len(list(dict_json.keys()))
+cam=bpy.data.collections[0].objects[2]
+def p(i):
+    bpy.ops.screen.animation_play()#start animation
+    # cam.rotation_euler=[1,1,1]
+    # cam.keyframe_insert(data_path="rotation_euler",frame=1)
+    # cam.rotation_euler=[2,2,2]
+    # cam.keyframe_insert(data_path="rotation_euler",frame=50)
+    # cam.rotation_euler=[3,3,3]
+    # cam.keyframe_insert(data_path="rotation_euler",frame=100)
+    if list_actions[i]=='X_velocity':
+        cam.location[0]=int(dict_json[list_actions[i]])
+        cam.keyframe_insert(data_path="location",frame=(i+1)*24)
+        print("x")
+    if list_actions[i]=='Y_velocity':
+        cam.location[1]=int(dict_json[list_actions[i]])
+        cam.keyframe_insert(data_path="location",frame=(i+1)*24)
+        print("y")
+    if list_actions[i]=='Z_velocity':
+        cam.location[2]=int(dict_json[list_actions[i]])
+        cam.keyframe_insert(data_path="location",frame=(i+1)*24)
+        print("z")
+    if list_actions[i]=='pitch':
+        cam.rotation_euler[0]=float(dict_json[list_actions[i]])
+        cam.keyframe_insert(data_path="rotation_euler",frame=(i+1)*24)
+        print("p")
+    if list_actions[i]=='yaw':
+        cam.rotation_euler[1]=float(dict_json[list_actions[i]])
+        cam.keyframe_insert(data_path="rotation_euler",frame=(i+1)*24)
+        print("y")
+    if list_actions[i]=='roll':
+        cam.rotation_euler[2]=float(dict_json[list_actions[i]])
+        cam.keyframe_insert(data_path="rotation_euler",frame=(i+1)*24)
+        print("r")
+for i in range(n):
+    p(i)
+#print(n)
+#print(list_actions)
+#print(dict_json)
